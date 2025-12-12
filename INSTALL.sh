808b8b340dcd10833f1079aeea5f52d1c86dd2a8
bpenaud <benjamin.penaud@umontpellier.fr>
2025-12-12 15:58:40 +0100

    Add a INSTALL shell script to donwload apptainer image and prepare working directory.

---
+++ INSTALL.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
+
+cd $SCRIPT_DIR
+
+echo -e "\n"
+echo "#################### Container preparation :"
+echo "Create Container directory in $SCRIPT_DIR"
+mkdir -p Container/Braker/
+mkdir -p Container/Busco/
+mkdir -p Container/Miniprot_Scipio/
+mkdir -p Container/Cleaning/
+echo -e "\n"
+
+echo "Download apptainer image in appropriate Container directory :"
+echo -e "\n"
+echo -e "Downloading Busco image :\n"
+wget https://cloud.isem-evolution.fr/nextcloud/index.php/s/55NHzLwoynfz2Wy/download/Busco.sif -O Container/Busco/Busco.sif
+echo -e "\n"
+echo -e "Downloading masking Tool image :\n"
+wget https://cloud.isem-evolution.fr/nextcloud/index.php/s/8HqJMTLaGNEnoNT/download/repeatM.sif -O Container/Braker/repeatM.sif
+echo -e "\n"
+echo -e "Downloading Miniprot and Scipio image :\n"
+wget https://cloud.isem-evolution.fr/nextcloud/index.php/s/CZ73RmQZ6rBWKNH/download/miniprot_scipio.sif -O Container/Miniprot_Scipio/miniprot_scipio.sif
+echo -e "\n"
+echo -e "Downloading Orthology and cleaing tools image :\n"
+wget https://cloud.isem-evolution.fr/nextcloud/index.php/s/RqbBcTraJ9bbABb/download/cleaning_orthology.sif -O Container/Cleaning/cleaning_orthology.sif
+
+echo -e "\n\n\n"
+
+echo "#################### Prepare working directory for CASIO pipeline :"
+echo "Create directory to set genome at format Genus_species.fasta : Data/Genome/"
+mkdir -p Data/Genome/
+echo -e "\n"
+echo "Now, you can download and put your Queries (Genus_species.fasta and Genus_species.gff) in Data/Query/"
+
+echo -e "\n\n\n"
+echo "Create directory to set query/queries at format Genus_species.fasta and Genus_species.gff : Data/Query/"
+echo -e "\n"
+mkdir -p Data/Query/
+
+echo "Now you can download and put your Queries (Genus_species.fasta and Genus_species.gff) in Data/Query/"
+
+echo -e "\n\n\n"
+echo "Create directory to set OrthoDB Protein database for Braker (https://bioinf.uni-greifswald.de/bioinf/partitioned_odb12/): Data/Protein_Database/"
+mkdir -p Data/Protein_Database/
+echo -e "\n"
+echo "Now, if you will use Braker you have to download a orthoDB protein (https://bioinf.uni-greifswald.de/bioinf/partitioned_odb12/) and put it in  Data/Protein_Database/"
+
+echo -e "\n\n\n"
+echo "#################### Last things to do :"
+echo "- Download and put Genomes, queries and protein database like explain above."
+echo "- Install snakemake and apptainer."
+echo "- Configure the ${SCRIPT_DIR}/configparam.yaml file."
+echo "- Execute the workflow with optionaly --singularity-args to bind data and results if there are not in CASIO directory:"
+echo "snakemake -s CASIO.smk --configfile configparam.yaml -c {threads} --use-singularity (--singularity-args '-B /path/where/are/data -B /path/where/are/queries/ -B /path/where/are/protein_database -B /path/where/write/results/')"
+
+
+
